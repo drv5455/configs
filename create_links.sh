7e78b08d6bf5cbc99370a6f45ad997441d53ff63
Daniel Vincelette <dvincelette@burne.twcny.rr.com>
2017-12-30 10:31:12 -0500

    Updates from testing on burne

--- create_links.sh
+++ create_links.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-ln -s .vimrc ../.vimrc
-ln -s .tmux.conf ../.tmux.conf
-ln -s .bashrc ../.bashrc
+PWD=`pwd`
+if [ -f ~/.vimrc ]; then
+	mv ~/.vimrc ~/.vimrc.old
+fi
+ln -s ${PWD}/.vimrc ~/.vimrc
+if [ -f ~/.tmux.conf ]; then
+	mv ~/.tmux.conf ~/.tmux.conf.old
+fi
+ln -s ${PWD}/.tmux.conf ~/.tmux.conf
+if [ -f ~/.bashrc ]; then
+	mv ~/.bashrc ~/.bashrc.old
+fi
+ln -s ${PWD}/.bashrc ~/.bashrc
---
+++ setup_basics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo dnf install -y meld            
+sudo dnf install -y vim             
+sudo dnf install -y powerline       
+sudo dnf install -y tmux            
+sudo dnf install -y vim-powerline   
+sudo dnf install -y tmux-powerline  
+sudo dnf install -y gvim   
+sudo dnf install -y git-svn
+sudo dnf install -y htop   
+sudo dnf install -y virt-manager
+sudo dnf install -y libvirt-client
+sudo dnf install -y iperf         
+sudo dnf install -y nfs-utils
+sudo dnf install -y libguestfs-tools
